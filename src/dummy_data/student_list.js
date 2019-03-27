3d0a9271c9f727842532974a6b42ed60fe7510ff
I Chun Lai <jenniferlai.81@gmail.com>
2019-03-27 12:54:38 -0700

    completed displaying dummy student data

---
+++ src/dummy_data/student_list.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        name: 'Clark Kent',
+        course: 'Flying 101',
+        grade: '98',
+        id: 1
+    },
+    {
+        name: 'George Fu',
+        course: 'Math 403',
+        grade: '87',
+        id: 2
+    },
+    {
+        name: 'Mary McWilliams',
+        course: 'Spanish 201',
+        grade: '45',
+        id: 3
+    },
+    {
+        name: 'Sophia Bode',
+        course: 'Greek 100',
+        grade: '76',
+        id: 4
+    },
+    {
+        name: 'Corey Jones',
+        course: 'Photography 302',
+        grade: '99',
+        id: 5
+    },
+];
