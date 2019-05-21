9c26f2324ea871abc7b24513b8d6090f41918c00
Jennifer Lai <jenniferlai.81@gmail.com>
2019-05-21 16:43:59 -0700

    created dummy data and created matching data in database

--- src/dummy_data/student_list.js
+++ src/dummy_data/student_list.js
@@ -1,32 +1,50 @@
 export default [
     {
-        name: 'Clark Kent',
-        course: 'Flying 101',
-        grade: '98',
+        completed: 0,
+        item: 'strawberries',
+        store: 'Ralph\'s',
+        unit_price: 125,
+        unit: 'lb',
         id: 1
     },
     {
-        name: 'George Fu',
-        course: 'Math 403',
-        grade: '87',
+        completed: 0,
+        item: 'Cauliflower (Avg. 1.99lbs)',
+        store: 'Sprouts',
+        unit_price: 149,
+        unit: 'lb',
         id: 2
     },
     {
-        name: 'Mary McWilliams',
-        course: 'Spanish 201',
-        grade: '45',
+        completed: 0,
+        item: 'Peach Yellow Whole Trade Guarantee Organic',
+        store: 'Whole Foods',
+        unit_price: 399,
+        unit: 'lb',
         id: 3
     },
     {
-        name: 'Sophia Bode',
-        course: 'Greek 100',
-        grade: '76',
+        completed: 0,
+        item: 'strawberries',
+        store: 'Albertsons',
+        unit_price: 249,
+        unit: 'pk',
         id: 4
     },
     {
-        name: 'Corey Jones',
-        course: 'Photography 302',
-        grade: '99',
+        completed: 0,
+        item: 'Chobani Fat Free Peach Greek Style Yogurt, 5.3 OZ',
+        store: 'Sprouts',
+        unit_price: 99,
+        unit: 'ea',
         id: 5
+    },
+    {
+        completed: 0,
+        item: 'Silk Unsweetened Pure Almond Milk',
+        store: 'Ralph\'s',
+        unit_price: 658,
+        unit: 'gal',
+        id: 6
     },
 ];
