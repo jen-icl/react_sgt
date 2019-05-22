e847615b499654bd534ca644c864555babfcdd5a
Jennifer Lai <jenniferlai.81@gmail.com>
2019-05-22 16:00:23 -0700

    server.js set up to connect with grocery_list db set up credential file created get endpoint checked connection on postman

--- public/api/server.js
+++ public/api/server.js
@@ -1,53 +1,34 @@
-//global process, node equivalent of window
-//to see on browser, run server (Terminal: 'npm test') and type url: 'localhost:3001/'
-
-//require is function that loads a library
-
-const express = require('express');//load the express library into the file
-const mysql = require('mysql'); //load the mysql library
-const mysqlcredentials = require('./mysqlcreds.js'); //load the credentials from a local file for mysql
+const express = require('express');
+const mysql = require('mysql');
+const mysqlcredentials = require('./mysqlcreds.js');
 const cors = require('cors');
 const PORT = process.env.PORT || 3001;
 
-//using the credentials that we loaded, establish a preliminary connection to the database
-const db = mysql.createConnection( mysqlcredentials );
-
 const server = express();
+const db = mysql.createConnection(mysqlcredentials);
 
-//middleware
 server.use(cors());
-server.use(express.static(__dirname + '../'));
-server.use(express.urlencoded( {extended: false}) ); //have express pull body data that is urlencoded and place it into an object called "body"
-server.use(express.json()); // used for things like axios
+server.use(express.json());
 
+server.get('/api/groceries', (req, res) => {
+    db.connect(() => {
+        const query = 'SELECT * FROM `grocery`';
 
-//make an endpoint to handle retrieving the grades of all students, get method
-server.get('/api/grades', (req, res) => {
-    //establish the connection to the database, and call the callback function when connection is made
-    db.connect( ()=> {
-        //create a query for our desired operation
-        const query = 'SELECT `id`, CONCAT(`givenname`," ", `surname`) AS `name`, `course`, `grade` FROM `grades`';
-        //send the query to the database, and call the given callback function once the data is retrieved or an error happens
-        db.query( query, (error, data)=>{
-            //if error is null, no error occurred = success
-            //create an output object to be sent back to the client
+        db.query(query, (error, data) => {
             const output = {
                 success: false,
             };
-            //if error is null, send the data
+
             if(!error){
-                //notify the client that we were successful
                 output.success = true;
-                //attach the data from the database to the output object
                 output.data = data;
             } else {
-                //an error occurred, attach that error onto the output so we can see what happened
                 output.error = error;
             }
-            //send the data back to the client
+
             res.send( output );
-        })
-    } )
+        });
+    });
 });
 
 
@@ -113,5 +94,5 @@ server.delete('/api/grades/:student_id', (request, response) => {
 
 server.listen(PORT, ()=>{
     //console.log('server is running on port 3001');
-    console.log('carrier has arrived');
+    console.log('carrier has arrived on port', PORT);
 });
